4f3df94f7c92d67f8e5f3161b806ebc28e055fcf
podestal <luisrodriguezpodesta@gmail.com>
2023-10-24 11:08:30 -0400

    black-jack-console mvp

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # black-jack-console
 
 This is a black jack game in a console built in Python
+The dealer does not add cards just the player
+When the player stop hitting, dealers cards show up
+An the winner is determined
--- main.py
+++ main.py
@@ -2,6 +2,36 @@
 
 import random
 
+def evaluate(playerSum, dealer):
+    if playerSum == 21:
+        print('You win')
+    elif playerSum > 21: 
+        print('You lost')
+    else:
+        playerHit = input('Hit? y/n')
+        if playerHit == 'y':
+            return playerHit
+        else:
+            print('Dealer cards: ', dealer[0], dealer[1])
+            if playerSum > dealerSum:
+                print('You win')
+            elif playerSum == dealerSum:
+                print('even')
+            else:
+                print('You lost')
+            return playerHit
+    
+def game(dealer, playerSum, player):
+    print(dealer[0], 'x')
+    print('These are your cards', playerSum)
+    print(player)
+
+def isHit(playerSum, player, cards):
+    newCard = cards[random.randint(0, len(cards) - 1)]
+    player.append(newCard)
+    playerSum += newCard
+    return playerSum
+
 print('Welcome to Jack Black')
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
@@ -20,18 +50,18 @@ playerSum = 0
 for card in player:
     playerSum += card
 
-print(dealer[0], 'x')
-print('These are your cards', playerSum)
-print(player)
+for card in dealer:
+    dealerSum += card
 
-def isHit(playerSum):
-    newCard = cards[random.randint(0, len(cards) - 1)]
-    player.append(newCard)
-    playerSum += newCard
-    return playerSum
 
-playerSum = isHit(playerSum)
-print('These are your cards', playerSum)
-print(player)
+game(dealer, playerSum, player)
 
-# This is a comment in my project
+playerHit = evaluate(playerSum, dealer)
+
+while playerHit == 'y':
+    playerSum = isHit(playerSum, player, cards)
+    game(dealer, playerSum, player)
+    playerHit = evaluate(playerSum, dealer)
+
+print(playerSum)
+    
